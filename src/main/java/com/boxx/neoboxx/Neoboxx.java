ff1a075d7ec60a317867234d42255998f6862d10
Boxxedboi <minefooox@gmail.com>
2024-03-23 15:46:10 -0600

    Initial commit :3

---
+++ src/main/java/com/boxx/neoboxx/Neoboxx.java
@@ -0,0 +1,42 @@
+package com.boxx.neoboxx;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.MapColor;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+import net.neoforged.neoforge.registries.DeferredBlock;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(Neoboxx.MODID)
+public class Neoboxx
+{
+    public static final String MODID = "neoboxx";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public Neoboxx(IEventBus modEventBus)
+    {
+
+
+        NeoForge.EVENT_BUS.register(this);
+    }
+}
---
+++ src/main/java/com/boxx/neoboxx/NeoboxxClient.java
@@ -0,0 +1,9 @@
+package com.boxx.neoboxx;
+
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = Neoboxx.MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public class NeoboxxClient {
+
+}
